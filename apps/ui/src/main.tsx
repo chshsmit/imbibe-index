c55172825cf055f06cb30c54ac7097105df4c772
Christopher Smith <chshsmit@gmail.com>
2023-10-27 15:42:14 -0700

    Got the initial ui library working

--- apps/ui/src/main.tsx
+++ apps/ui/src/main.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { NextUIProvider } from "@nextui-org/react";
+import { ThemeProvider } from "next-themes";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <NextUIProvider>
+      <ThemeProvider attribute="class" defaultTheme="dark">
+        <App />
+      </ThemeProvider>
+    </NextUIProvider>
   </React.StrictMode>,
-)
+);
