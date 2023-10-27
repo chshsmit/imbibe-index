7a10fd8800ae2c92f2620479eb66916a827e864e
Christopher Smith <chshsmit@gmail.com>
2023-10-27 14:58:31 -0700

    More setup

--- apps/api/dist/index.js
+++ apps/api/dist/index.js
@@ -3,12 +3,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = __importDefault(require("express"));
-var app = (0, express_1.default)();
-var port = 3000;
-app.get("/", function (req, res) {
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+const port = 3000;
+app.get("/", (req, res) => {
     res.send("Hello, express with typescript");
 });
-app.listen(port, function () {
-    console.log("Server is running on port ".concat(port));
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
